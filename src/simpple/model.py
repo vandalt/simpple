d93e208506e52f2fc03ed550e0d09e028abb8b72
Thomas Vandal <thomas.vandal@umontreal.ca>
2025-08-27 21:10:14 -0400

    Add model

---
+++ src/simpple/model.py
@@ -0,0 +1,109 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+from typing import TYPE_CHECKING
+
+import numpy as np
+from numpy.typing import ArrayLike
+
+if TYPE_CHECKING:
+    from nautilus import Prior
+
+
+class Model:
+    def __init__(self, parameters: dict, log_likelihood: Callable):
+        self.parameters = parameters
+        self._log_likelihood = log_likelihood
+        # Attempt to make log-likelihood inherit docstring.
+        # Works at runtime but not for static (LSP) tools
+        self.log_likelihood.__func__.__doc__ = self._log_likelihood.__doc__
+
+    def _log_likelihood(self, parameters, *args, **kwargs) -> float:
+        raise NotImplementedError(
+            "log_likelihood must be passed to init or _log_likelihood must be "
+            "implemented by subclasses."
+        )
+
+    def keys(self) -> list[str]:
+        """Get a list of parameter names"""
+        return list(self.parameters.keys())
+
+    def log_likelihood(self, parameters, *args, **kwargs):
+        if not isinstance(parameters, dict):
+            parameters = dict(zip(self.keys(), parameters, strict=True))
+        return self._log_likelihood(parameters, *args, **kwargs)
+
+    def log_prior(self, parameters: dict | ArrayLike) -> float:
+        """Log of the prior probability for the model
+
+        :param parameters: Dictionary or array of parameters. If an array is used,
+                           the order must be the same as `Model.keys()`
+        :return: Log-prior probability
+        """
+        if not isinstance(parameters, dict):
+            parameters = dict(zip(self.keys(), parameters, strict=True))
+        lp = 0.0
+        for pname, pval in parameters.items():
+            pdist = self.parameters[pname]
+            lp += pdist.log_prob(pval)
+        return lp
+
+    def prior_transform(self, u: ArrayLike | dict) -> np.ndarray | dict:
+        """Prior transform of the model
+
+        Takes samples from a uniform distribution between 0 and 1
+        for all parameters and returns samples transformed according to the prior.
+
+        :param u: Samples from the uniform distribution. Can be a dict or an array
+                  ordered as `Model.keys()`
+        :return: Prior samples, as a dict or an array depending on the input type.
+        """
+        is_dict = isinstance(u, dict)
+        if is_dict:
+            u = np.array(list(u.values()))
+        x = np.array(u)
+        for i, pdist in enumerate(self.parameters.values()):
+            x[i] = pdist.prior_transform(u[i])
+        if is_dict:
+            x = dict(zip(self.keys(), x, strict=True))
+        return x
+
+    def nautilus_prior(self) -> "Prior":
+        """Builds and return a `nautilus.Prior` for the model.
+
+        :return: Nautilus Prior object.
+        """
+        from nautilus import Prior
+
+        prior = Prior()
+        for pname, pdist in self.parameters.items():
+            prior.add_parameter(pname, pdist.dist)
+        return prior
+
+    def log_prob(self, parameters: dict | ArrayLike, *args, **kwargs) -> float:
+        """Log posterior probability for the model
+
+        :param parameters: Parameters as a dict or an array ordered as `Model.keys()`
+        :return: Log posterior probability
+        """
+        if not isinstance(parameters, dict):
+            parameters = dict(zip(self.keys(), parameters, strict=True))
+        lp = self.log_prior(parameters)
+        if np.isfinite(lp):
+            return lp + self.log_likelihood(parameters, *args, **kwargs)
+        return lp
+
+    def get_prior_samples(self, n_samples: int, fmt: str = "dict") -> dict:
+        """Generate prior samples for all parameters
+
+        :param n_samples: Number of samples
+        :param fmt: Format of the samples (dict or array)
+        :return: Dictionary of prior samples
+        """
+        rng = np.random.default_rng()
+        u = rng.uniform(size=(len(self.parameters), n_samples))
+        if fmt == "dict":
+            u = dict(zip(self.keys(), u, strict=True))
+        elif fmt != "array":
+            raise ValueError(f"Invalid format: {fmt}. Use 'dict' or 'array'.")
+        return self.prior_transform(u)
