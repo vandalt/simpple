3d9a0125a765cf0df2b92196e23f8b6514c56ee1
Thomas Vandal <thomas.vandal@umontreal.ca>
2025-08-29 17:05:42 -0400

    Comment about version in editable installs

--- tests/test_simpple.py
+++ tests/test_simpple.py
@@ -1,5 +1,8 @@
 import simpple
 
+
 def test_simpple_version():
     from importlib.metadata import version
+
+    # For editable installs, you might just need to re-install if this fails
     assert simpple.__version__ == version("simpple")
